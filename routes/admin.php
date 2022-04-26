fbf26c5ff9402065e33e886c3ad7091aec3bf947
Irelene <irelene.dev@gmail.com>
2022-04-26 12:35:48 +0800

    Disable admin side at the moment

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-    Route::resource('admin/positions', 'Admin\PositionController');
-    Route::resource('admin/candidates', 'Admin\CandidateController');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::resource('admin/positions', 'Admin\PositionController');
+//     Route::resource('admin/candidates', 'Admin\CandidateController');
+// });
